fd304e48d3e63054be1c01a9743b18af2a2f81b9
maetin0324 <maetin0324@gmail.com>
2025-11-11 12:48:28 +0900

    release build

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ fi
 
 # Build BenchFS library
 echo -e "${YELLOW}Building BenchFS library...${NC}"
-cargo build --lib
+cargo build --release --lib
 
 if [ $? -ne 0 ]; then
     echo -e "${RED}Error: Failed to build BenchFS library${NC}"
